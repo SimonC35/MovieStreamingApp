66cdbc992ad380fe5a3d0177cb955572b4940d01
Simon <sim.coatanlem.35@gmail.com>
2026-02-05 10:59:44 +0100

    Initial Commit

---
+++ MovieStreamingAppTests/MovieStreamingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStreamingAppTests.swift
+//  MovieStreamingAppTests
+//
+//  Created by Cours on 05/02/2026.
+//
+
+import Testing
+@testable import MovieStreamingApp
+
+struct MovieStreamingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
